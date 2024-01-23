e0c58fd78f17da4c34fc4ff9baedec118bcf6421
heogeonho <geonh1016@gmail.com>
2024-01-23 23:03:12 +0900

    feat: ArticleRepository 활용 -> 엔티티 DB에 저장
    CrudRepository 인터페이스 활용
    @Autowired 어노테이션 학습

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -2,12 +2,17 @@ package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+    @Autowired // 스프링 부트가 미리 생성해 놓은 리파지터리 객체 주입(DI) -> 의존성 주입 Dependency Injection
+    private ArticleRepository articleRepository;
+    // = new ArticleRepositoryImpl() 하지 않는 이유: 스프링 부트는 알아서 객체 만듦
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -19,7 +24,10 @@ public class ArticleController {
         System.out.println(form.toString());
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
+        System.out.println(article.toString());
         // 2. 리파지터리로 엔티티 DB에 저장
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
         return "";
     }
 }
