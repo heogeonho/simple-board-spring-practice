b27555383523fd847873a5f9fe8a9ad315f1ee0a
heogeonho <geonh1016@gmail.com>
2024-01-25 14:34:37 +0900

    feat: 전체 글 조회 및 출력

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import java.util.ArrayList;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -44,5 +45,15 @@ public class ArticleController {
         model.addAttribute("article", articleEntity);
         // 3. 뷰 페이지 반환하기
         return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // 1. 모든 데이터 가져오기
+        ArrayList<Article> articleEntitiyList = articleRepository.findAll();
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntitiyList);
+        // 3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -1,9 +1,12 @@
 package com.example.firstproject.repository;
 
 import com.example.firstproject.entity.Article;
+import java.util.ArrayList;
 import org.springframework.data.repository.CrudRepository;
 
 // CrudRepository는 JPA에서 제공하는 인터페이스 -> 이를 상속해 엔티티를 관리할 수 있음
 // 현재 상속 받아 사용하기 때문에 기본 동작은 활용 가능
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
