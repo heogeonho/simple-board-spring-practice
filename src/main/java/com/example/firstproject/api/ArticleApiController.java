ff74e4ce34843127b95c82acf1d0e061247f28da
heogeonho <geonh1016@gmail.com>
2024-02-03 22:03:01 +0900

    feat: 게시글 조회하기

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -5,6 +5,7 @@ import com.example.firstproject.repository.ArticleRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +16,11 @@ public class ArticleApiController {
     @GetMapping("/api/articles")
     public List<Article> index() {
         return articleRepository.findAll();
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public Article show(@PathVariable Long id) {
+        return articleRepository.findById(id).orElse(null);
     }
     // POST
     // PATCH
