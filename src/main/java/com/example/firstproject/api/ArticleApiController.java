7954949e71ea635943ccfe19abcec66646e501e7
heogeonho <geonh1016@gmail.com>
2024-02-14 20:20:00 +0900

    트랜잭션 테스트 진행 @Transactional

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -60,4 +60,13 @@ public class ArticleApiController {
                 ResponseEntity.status(HttpStatus.NO_CONTENT).build() :
                 ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
+
+    // 트렌잭션 테스트
+    @PostMapping("/api/transaction-test")
+    public ResponseEntity<List<Article>> transactionTest (@RequestBody List<ArticleForm> dtos) {
+        List<Article> createdList = articleService.creatArticles(dtos);
+        return (createdList != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(createdList) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 }
